9a6096f1e19cdfcc1984a9b39126d96f8e917460
Alex Yang <alex.hyang.dev@gmail.com>
2023-04-13 01:38:54 -0700

    optimize src/ for project initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import Nav from 'components/Nav'
+import Main from 'components/Main'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Nav />
+      <Main />
+      <header className="App-header"></header>
       <div className="container">
         <div>First Div</div>
         <div>Second Div</div>
         <div className="player">
-        <video controls muted>
-          <source src="http://localhost:4000/video/test" type="video/mp4"></source>
-        </video>
+          <video controls muted>
+            <source
+              src="http://localhost:4000/video/test"
+              type="video/mp4"
+            ></source>
+          </video>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
